f9b14c575c6ad8fb071d3c3c8bfcdc5d0d54b4f4
RaylonNogueira <141072418+RaylonNogueira@users.noreply.github.com>
2023-09-04 15:59:17 -0400

    Update visita.js

--- visita.js
+++ visita.js
@@ -1,1 +1,43 @@
-
+document.addEventListener("DOMContentLoaded", function () {
+    const gallery = document.querySelector('.gallery');
+    const prevButton = document.querySelector('.prev');
+    const nextButton = document.querySelector('.next');
+    const lightbox = document.querySelector('.lightbox');
+    const lightboxImage = document.querySelector('.lightbox-image');
+    const closeLightbox = document.querySelector('.close');
+    const imageContainers = Array.from(document.querySelectorAll('.image-container img'));
+    let lightboxIndex = 0;
+  
+    function openLightbox(index) {
+      lightboxImage.src = imageContainers[index].src;
+      lightbox.style.display = 'flex';
+      lightboxIndex = index;
+    }
+  
+    closeLightbox.addEventListener('click', function () {
+      lightbox.style.display = 'none';
+    });
+  
+    prevButton.addEventListener('click', function () {
+      lightboxIndex = (lightboxIndex - 1 + imageContainers.length) % imageContainers.length;
+      lightboxImage.src = imageContainers[lightboxIndex].src;
+    });
+  
+    nextButton.addEventListener('click', function () {
+      lightboxIndex = (lightboxIndex + 1) % imageContainers.length;
+      lightboxImage.src = imageContainers[lightboxIndex].src;
+    });
+  
+    imageContainers.forEach((image, index) => {
+      image.addEventListener('click', () => {
+        openLightbox(index);
+      });
+    });
+  
+    lightbox.addEventListener('click', function (event) {
+      if (event.target.classList.contains('lightbox')) {
+        lightbox.style.display = 'none';
+      }
+    });
+  });
+  
